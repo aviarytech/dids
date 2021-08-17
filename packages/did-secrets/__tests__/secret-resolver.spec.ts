7e54abc902f4cdfc1d9961f6025c60b9b8413e4c
Brian Richter <brianorwhatever@gmail.com>
2021-08-17 01:22:26 -0700

    tests pass

--- packages/did-secrets/__tests__/secret-resolver.spec.ts
+++ packages/did-secrets/__tests__/secret-resolver.spec.ts
@@ -1,19 +1,43 @@
+import { SecretTypeNotFound } from "../src/Secret";
 import { JSONSecretResolver } from "../src";
 
-test("secret resolver can resolve a JSON file", async () => {
+test("secret resolver can resolve a JsonWebKey2020 JSON file", async () => {
   const secretFile = require("../__fixtures__/JsonWebKey2020.example.json");
 
   const resolver = new JSONSecretResolver(secretFile);
   const secret = await resolver.resolve(secretFile["id"]);
+  const jwk = secret.asJwk();
 
   expect(secret.id).toBe("did:web:example.com#key-0");
   expect(secret.type).toBe("JsonWebKey2020");
-  expect(secret.privateKeyJwk.x).toBe(
-    "s_7sGeMPuhusy_X4slKydGXWAhvVfqBDW2DwZHloWr0"
+  expect(jwk.crv).toBe("Ed25519");
+  expect(jwk.d).toBe("u7OKM36_b8k4Yk6QI0c_lOznRsKwnOzlhTfqCkr6VmY");
+  expect(jwk.kty).toBe("OKP");
+});
+
+test("secret resolver can resolve a X25519KeyAgreementKey2019 JSON file", async () => {
+  const secretFile = require("../__fixtures__/X25519KeyAgreementKey2019.example.json");
+  const resolver = new JSONSecretResolver(secretFile);
+
+  const secret = await resolver.resolve(secretFile["id"]);
+  const jwk = secret.asJwk();
+
+  expect(secret.id).toBe("did:web:example.com#key-1");
+  expect(secret.type).toBe("X25519KeyAgreementKey2019");
+  expect(secret.privateKeyBase58).toBe(
+    "66pGmEHd7fBfQa9ap27vWSouHAmipbmmw6GduBwNRY6y"
   );
-  expect(secret.privateKeyJwk.crv).toBe("Ed25519");
-  expect(secret.privateKeyJwk.d).toBe(
-    "u7OKM36_b8k4Yk6QI0c_lOznRsKwnOzlhTfqCkr6VmY"
-  );
-  expect(secret.privateKeyJwk.kty).toBe("OKP");
+  expect(jwk.crv).toBe("X25519");
+  expect(jwk.kty).toBe("OKP");
+});
+
+test("secret throws error when doesn't support a key type", async () => {
+  try {
+    const resolver = new JSONSecretResolver({
+      id: "did:web:example.com#key-1",
+      type: "BAR",
+    });
+  } catch (e) {
+    expect(e instanceof SecretTypeNotFound).toBeTruthy();
+  }
 });
--- packages/did-secrets/src/Secret.ts
+++ packages/did-secrets/src/Secret.ts
@@ -1,4 +1,8 @@
-import { JWK } from "@aviarytech/crypto-core";
+import {
+  JWK,
+  X25519KeyAgreementKey,
+  JsonWebKey2020,
+} from "@aviarytech/crypto-core";
 import { ISecret } from "./interfaces";
 
 export class SecretTypeAsJWKNotSupportedError extends Error {
@@ -11,7 +15,7 @@ export class SecretTypeAsJWKNotSupportedError extends Error {
 export class SecretTypeNotFound extends Error {
   constructor(type: string) {
     super(type);
-    this.message = `Secret ${type} not supported`;
+    this.message = `Secret type "${type}" not supported`;
   }
 }
 
@@ -19,24 +23,50 @@ export class Secret implements ISecret {
   id: string;
   type: string;
   privateKeyPem?: string;
+  publicKeyPem?: string;
   privateKeyJwk?: any;
+  publicKeyJwk?: any;
   privateKeyHex?: string;
+  publicKeyHex?: string;
   privateKeyBase64?: string;
+  publicKeyBase64?: string;
   privateKeyBase58?: string;
+  publicKeyBase58?: string;
   privateKeyMultibase?: string;
+  publicKeyMultibase?: string;
+
+  private key: JsonWebKey2020 | X25519KeyAgreementKey;
 
   constructor(document: object) {
     this.id = document["id"];
     this.type = document["type"];
+
     switch (this.type) {
       case "JsonWebKey2020":
+        this.publicKeyJwk = document["publicKeyJwk"];
         this.privateKeyJwk = document["privateKeyJwk"];
+        this.key = new JsonWebKey2020(
+          this.id,
+          null,
+          this.publicKeyJwk,
+          this.privateKeyJwk
+        );
+        return this;
+      case "X25519KeyAgreementKey2019":
+        this.publicKeyBase58 = document["publicKeyBase58"];
+        this.privateKeyBase58 = document["privateKeyBase58"];
+        this.key = new X25519KeyAgreementKey(
+          this.id,
+          null,
+          this.publicKeyBase58,
+          this.privateKeyBase58
+        );
         return this;
     }
     throw new SecretTypeNotFound(this.type);
   }
 
   asJwk(): JWK {
-    throw new SecretTypeAsJWKNotSupportedError(this.type);
+    return this.key.asJwk();
   }
 }
