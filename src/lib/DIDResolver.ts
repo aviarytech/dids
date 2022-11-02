77e8a97595e1828debfb09263f5cece1c5968308
Brian Richter <brianorwhatever@gmail.com>
2022-11-01 22:48:36 -0700

    did resolver works

--- src/lib/DIDResolver.ts
+++ src/lib/DIDResolver.ts
@@ -1,66 +1,65 @@
-export const todo = 1;
-// import { documentLoaderFactory } from "@transmute/jsonld-document-loader";
-// import { DocumentLoader } from "@transmute/jsonld-document-loader/dist/types";
-// import axios from "axios";
+import { documentLoaderFactory, type Iri } from "@transmute/jsonld-document-loader";
+import type { DocumentLoader } from "@transmute/jsonld-document-loader";
+import axios from "axios";
 
-// import { DIDDocument } from "./DIDDocument";
+import { DIDDocument } from "./DIDDocument";
 
-// export class DIDNotFoundError extends Error {
-//   constructor(did: string) {
-//     super(did);
-//     this.message = `DID ${did} not found`
-//   }
-// }
+export class DIDNotFoundError extends Error {
+  constructor(did: string) {
+    super(did);
+    this.message = `DID ${did} not found`
+  }
+}
 
-// export class DIDMethodNotSupported extends Error {
-//   constructor(method: string) {
-//     super(method)
-//     this.message = `DID method ${method} not supported by resolver`
-//   }
-// }
+export class DIDMethodNotSupported extends Error {
+  constructor(method: string) {
+    super(method)
+    this.message = `DID method ${method} not supported by resolver`
+  }
+}
 
-// export class DIDResolver {
-//   private documentLoader: DocumentLoader;
+export class DIDResolver {
+  private documentLoader: DocumentLoader;
 
-//   constructor() {
-//     this.createDocumentLoader();
-//   }
+  constructor() {
+    this.documentLoader = this.createDocumentLoader();
+  }
 
-//   private createDocumentLoader() {
-//     this.documentLoader = documentLoaderFactory.pluginFactory
-//       .build()
-//       .addResolver({
-//         ["did:web"]: {
-//           resolve: async (did: string) => {
-//             const [_, method, id, ...extras] = did.split(":");
-//             let domain = id.split("#").length > 1 ? id.split("#")[0] : id;
-//             if (id.indexOf("localhost") >= 0) {
-//               domain += `:${extras}`;
-//             }
-//             const resp = await axios.get(
-//               `http${
-//                 id.indexOf("localhost") >= 0 ? null : "s"
-//               }://${domain}/.well-known/did.json`
-//             );
-//             return resp.data;
-//           },
-//         },
-//       })
-//       .buildDocumentLoader();
-//   }
+  private createDocumentLoader() {
+    return documentLoaderFactory
+      .build({
+        ["did:web"]: async (did: string) => {
+          const [_, method, id, ...extras] = did.split(":");
+          let domain = id.split("#").length > 1 ? id.split("#")[0] : id;
+          if (id.indexOf("localhost") >= 0) {
+            domain += `:${extras}`;
+          }
+          const resp = await axios.get(
+            `http${
+              id.indexOf("localhost") >= 0 ? null : "s"
+            }://${domain}/.well-known/did.json`
+          );
+          return resp.data;
+        },
+      })
+  }
 
-//   async resolve(did: string): Promise<DIDDocument> {
-//     if (did.split(":").length < 3) {
-//       throw new Error(`${did} not a valid DID`)
-//     }
-//     try {
-//       const { document } = await this.documentLoader(did);
-//       if (!document) throw new DIDNotFoundError(did);
-//       return new DIDDocument(JSON.parse(document));
-//     } catch(e) {
-//       const [_, method ] = did.split(":")
-//       if (e.message.indexOf("documentLoader called on unsupported URI") >= 0) throw new DIDMethodNotSupported(`did:${method}`)
-//       throw e;
-//     }
-//   }
-// }
+  async resolve(did: Iri): Promise<DIDDocument> {
+    if (did.split(":").length < 3) {
+      throw new Error(`${did} not a valid DID`)
+    }
+    try {
+      const { document } = await this.documentLoader(did);
+      if (!document) throw new DIDNotFoundError(did);
+      if (typeof document === 'object') {
+        return new DIDDocument(document)
+      }
+      return new DIDDocument(JSON.parse(document));
+    } catch(e: any) {
+      const [_, method ] = did.split(":")
+      if (e.message.indexOf("Unsupported iri") >= 0) throw new DIDMethodNotSupported(`did:${method}`)
+      if (e.message.indexOf("status code 404") >= 0) throw new DIDNotFoundError(did)
+      throw e;
+    }
+  }
+}
--- tests/did-resolver.spec.ts
+++ tests/did-resolver.spec.ts
@@ -1,45 +1,46 @@
-// import { test, expect } from "vitest";
+import { beforeEach, describe, expect, test } from "vitest";
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter'
+import { DIDMethodNotSupported, DIDNotFoundError, DIDResolver } from "$lib";
 
-import { expect, test } from "vitest";
+var mock = new MockAdapter(axios);
 
+describe('did-resolver', () => {
+  beforeEach(() => {
+    mock.reset();
+  })
 
-// afterEach(() => {
-//   mockedAxios.get.mockReset();
-// });
-
-// test("did resolver can resolve a web did", async () => {
-//   const didDoc = require("../__fixtures__/didDocument.json");
-//   mockedAxios.get.mockResolvedValue({ data: JSON.stringify(didDoc) });
-
-//   const resolver = new DIDResolver();
-//   const did = await resolver.resolve("did:web:example.com");
-
-//   expect(axios.get).toHaveBeenCalled();
-//   expect(did.context).toContain("https://www.w3.org/ns/did/v1");
-// });
-
-// test("did resolver DIDNotFound when did not resolved", async () => {
-//   const resolver = new DIDResolver();
-//   mockedAxios.get.mockResolvedValue({ status: 404, statusText: "Not Found" });
-
-//   try {
-//     const did = await resolver.resolve("did:web:example.com");
-//   } catch (e) {
-//     expect(e instanceof DIDNotFoundError).toBeTruthy();
-//   }
-// });
-
-// test("did resolver throws not supported when did method not found", async () => {
-//   const resolver = new DIDResolver();
-//   console.error = jest.fn();
-
-//   try {
-//     const did = await resolver.resolve("did:fake:example.com");
-//   } catch (e) {
-//     expect(console.error).toHaveBeenCalled();
-//     expect(e instanceof DIDMethodNotSupported).toBeTruthy();
-//   }
-// });
-test('todo add tests', () => {
-  expect(true).toBeTruthy()
+  test("did resolver can resolve a web did", async () => {
+    const didDoc = require("./fixtures/didDocument.json");
+    mock.onGet().reply(200, JSON.stringify(didDoc), { headers: { 'Content-Type': 'application/json'}})
+  
+    const resolver = new DIDResolver();
+    const did = await resolver.resolve("did:web:example.com");
+  
+    expect(did.context).toContain("https://www.w3.org/ns/did/v1");
+  });
+  
+  test.only("did resolver DIDNotFound when did not resolved", async () => {
+    const resolver = new DIDResolver();
+    mock.onGet().reply(404, 'Not Found')
+  
+    try {
+      const did = await resolver.resolve("did:web:example.com");
+      expect(true).toBeFalsy()
+    } catch (e) {
+      expect(e instanceof DIDNotFoundError).toBeTruthy();
+    }
+  });
+  
+  test("did resolver throws not supported when did method not found", async () => {
+    const resolver = new DIDResolver();
+  
+    try {
+      const did = await resolver.resolve("did:fake:example.com");
+      expect(true).toBeFalsy()
+    } catch (e) {
+      console.log(e)
+      expect(e instanceof DIDMethodNotSupported).toBeTruthy();
+    }
+  });
 })
